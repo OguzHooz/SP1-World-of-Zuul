b768ee29a069bcfcf5c1dbb65cbd83e619cd4deb
Oguz <oguz2863@gmail.com>
2020-11-03 09:28:26 +0100

    Removed Eksempel

--- GruppeProjekt-master/WorldOfZuulGui/WorldOfZuulGui/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.worldofzuulgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.worldofzuulgui to javafx.fxml;
-    exports com.mycompany.worldofzuulgui;
-}
