6fe34afc7310f0ec1b63d9b06137b5a5dfc39b71
Louie <mail@louie.dk>
2020-11-30 14:43:34 +0100

    GUI start

--- oceanoftrashGUI/Main.java
+++ oceanoftrashGUI/Main.java
@@ -8,27 +8,19 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
 public class Main extends Application {
-    public class ButtonHelloWorldHandler implements EventHandler<ActiveEvent>{
-        @Override
-        public void handle (ActiveEvent event){
-            System.out.println("Hello World!");
-        }
-    }
+
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        Button btn = new Button();
-        btn.setLabel("Say 'Hello World'");
-        btn.setOnAction(new ButtonHelloWorldHandler());
-        StackPane root2 = new StackPane(btn);
-        Scene scene = new Scene(root2, 300, 250);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, 300,275));
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
